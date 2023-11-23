d4651c09643311a156ec807fbd8424a64eb7117f
JinyongJeong <jjy0923@gmail.com>
2023-11-23 10:18:35 +0900

    add auto installer

--- ubuntu22.04/script/driver_autoinstall.sh
+++ ubuntu22.04/script/driver_autoinstall.sh
@@ -1,1 +1,7 @@
+# This script automatically install drivers for ubuntu environment
+
+source ./../../common/color.sh
+
 sudo ubuntu-drivers autoinstall
+
+echo -e "${GREEN}If additional packages are installed, please reboot PC"
